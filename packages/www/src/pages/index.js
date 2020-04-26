7a84c51d60ee91a170e12016ba570cfbe8f37ec9
imalbert <albert.s.manuel@gmail.com>
2020-04-27 07:20:51 +0800

    Add auth with netlify-identity-widget

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,23 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
 import {
   Container,
   Heading,
   Button,
   Flex,
 } from 'theme-ui'
+import netlifyIdentity from 'netlify-identity-widget'
 
-export default props => (
-  <Container>
-    <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-      <Heading as='h1'>Get Stuff Done</Heading>
-      <Button
-        sx={{ marginTop: 2 }}
-        onClick={() => {
-          alert('clicked')
-        }}
-      >
-        Log In
-      </Button>
-    </Flex>
-  </Container>
-)
+export default props => {
+  useEffect(() => {
+    netlifyIdentity.init({})
+  })
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+        <Heading as='h1'>Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open()
+          }}
+        >
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  )
+}
