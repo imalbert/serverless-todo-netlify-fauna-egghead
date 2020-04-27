a3c0d34fa61772cc9c8997e4030725a4d93ae5e5
imalbert <albert.s.manuel@gmail.com>
2020-04-28 07:15:01 +0800

    Add todo type and resolvers to apollo function

--- functions/graphql-api/graphql-api.js
+++ functions/graphql-api/graphql-api.js
@@ -3,15 +3,38 @@ const { ApolloServer, gql } = require('apollo-server-lambda')
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    todos: [Todo]!
+  }
+  type Todo {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+  type Mutation {
+    addTodo(text: String!): Todo
+    updateTodoDone(id: ID!): Todo
   }
 `
 
+const todos = {}
+let todoIndex = 0
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    todos: () => Object.values(todos),
   },
+  Mutation: {
+    addTodo: (_, { text }) => {
+      todoIndex++
+      const id = `key-${todoIndex}`
+      todos[id] = { id, text, done: false }
+      return todos[id]
+    },
+    updateTodoDone: (_, { id }) => {
+      todos[id].done = true
+      return todos[id]
+    }
+  }
 }
 
 const server = new ApolloServer({
