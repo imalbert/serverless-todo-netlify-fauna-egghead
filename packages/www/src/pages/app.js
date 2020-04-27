176a0c187a5d9b4b0083bb909a375d5e34137392
imalbert <albert.s.manuel@gmail.com>
2020-04-27 11:37:44 +0800

    Add todo list with useReducer

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -8,6 +8,7 @@ import {
   NavLink,
 } from 'theme-ui'
 import { IdentityContext } from '../../identity-context'
+import Dashboard from './dashboard'
 
 let Dash = () => {
   const { user, identity } = useContext(IdentityContext)
@@ -17,28 +18,7 @@ let Dash = () => {
   }
 
   return (
-    <Container>
-      <Flex as='nav'>
-        <NavLink as={Link} to='/' p={2}>Home</NavLink>
-        <NavLink as={Link} to='/app' p={2}>Dashboard</NavLink>
-        {user && (
-          <NavLink href='#!' p={2}>
-            {user.user_metadata.full_name}
-          </NavLink>
-        )}
-      </Flex>
-      <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-        <Heading as='h1'>Get Stuff Done</Heading>
-        <Button
-          sx={{ marginTop: 2 }}
-          onClick={() => {
-            identity.open()
-          }}
-        >
-          Log In
-        </Button>
-      </Flex>
-    </Container>
+    <Dashboard/>
   )
 }
 
